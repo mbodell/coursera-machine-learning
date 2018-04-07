4498431c9eb236a50d6411736dfd299b47b24e1a
Michael Bodell <mbodell@gmail.com>
2018-04-07 03:58:40 -0700

    Find C and sigma values through CV

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -11,6 +11,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+const_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,7 +25,21 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+best_val_error = 10000000;
 
+for i = 1:length(const_vec)
+	c = const_vec(i);
+	for j = 1:length(const_vec)
+		s = const_vec(j);
+		model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+		val_error = mean(double(svmPredict(model, Xval) ~= yval));
+		if (val_error < best_val_error)
+			C = c;
+			sigma = s;
+			best_val_error = val_error;
+		endif
+	end
+end
 
 
 
